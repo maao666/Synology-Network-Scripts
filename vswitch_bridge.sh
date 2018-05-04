59fe9f5f79fcd86f8bbb6ea345d0b5671510817a
maao666 <maao666@icloud.com>
2018-05-04 11:14:38 -0700

    Initial Upload

---
+++ vswitch_bridge.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+start() {
+	DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+	STATUS="$(ovs-vsctl show | grep ovs_eth1)"
+	if [ "$STATUS" != "" ]
+	then
+		echo "Trying to enable bridging"
+		ovs-vsctl del-br ovs_eth1
+		ovs-vsctl add-port ovs_eth0 eth1
+		#rm $DIR/Latest-bridge-date.txt
+		date > $DIR/Latest-bridge-date.txt
+		for i in {1..5}
+		do
+			sleep 20
+			STATUS="$(ovs-vsctl show | grep ovs_eth1)"
+			if [ "$STATUS" != "" ]
+			then
+				echo "Run " $i+1 " times" >> $DIR/Latest-bridge-date.txt
+				ovs-vsctl del-br ovs_eth1
+				ovs-vsctl add-port ovs_eth0 eth1
+			else
+				exit 1
+			fi
+		done
+		STATUS="$(ovs-vsctl show | grep ovs_eth1)"
+		if [ "$STATUS" != "" ]
+			then
+				echo "Failed to bridge ports. Please check your settings." >> $DIR/Latest-bridge-date.txt
+				exit -1
+		fi
+	else
+		echo "$STATUS"
+		echo "Nothing to do. Exiting..."
+	fi
+}
+
+stop() {
+echo "Stopping"
+}
+
+case "$1" in
+  start)
+	start
+	;;
+  stop)
+	stop
+	;;
+  restart)
+	stop
+	start
+	;;
+  *)
+	echo "This script enable bridging between eth0 and eth1 with vSwitch"
+	echo "Usage: $0 {start|stop|restart}" >&2
+	exit 1
+	;;
+esac
